ce6fa96962c8c9e0296de5ff46bf747ae120bca4
marina belikova <memymilk@gmail.com>
2023-02-24 10:47:19 +0100

    WiP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # get 'movies/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: "pages#home"
+  resources :movies, only: [:index, :show] do
+    resources :bookmarks, only: [:create]
+  end
+
 end
